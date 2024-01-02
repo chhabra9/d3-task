392cae1dfab1fc151d211fce04d417960a63ed8b
Achint Singh Chhabra <achint@Achints-MacBook-Pro.local>
2024-01-02 23:16:45 +0530

    initial functionality added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(() => {
     let dim = {
         height: window.screen.height,
@@ -28,77 +29,78 @@ $(document).ready(() => {
 
         }
     });
+
     createTree = (data) => {
-        // d3.select(self.frameElement).style("height", "500px");
         let tree = d3.layout.tree().nodeSize([20, 30]).size([
             dim.height - margin.top - margin.bottom, dim.width - margin.left - margin.right
         ]);
 
         let nodes = tree.nodes(data).reverse();
         let links = tree.links(nodes);
-        console.log(nodes, links)
         let diagonal = d3.svg.diagonal().projection((d) => [d.y, d.x]);
-        nodes.forEach((d) => d.y = d.depth * 110)
+        nodes.forEach((d) => d.y = d.depth * 110);
+
         let svg = d3.select("body").
             append("svg").
             attr("width", dim.width).
             attr("height", dim.height).
             append("g").
             attr("transform", `translate(${margin.left},${margin.right})`);
+
         let i = 0;
+
         let nodeData = svg.selectAll("g.node").data(nodes, (d) => d.id || (d.id = ++i));
+
         var nodeEnter = nodeData.enter().append("g")
             .attr("class", "node")
-            .attr("transform", (d) => { return `translate(0,${data.startPoint})` })
+            .attr("transform", (d) => `translate(0,${data.startPoint})`)
             .on("click", (d) => {
-                click(d)
+                click(d, data);
             })
             .on("mouseover", (d) => {
+                let selectedNode = d3.selectAll("circle").filter(p => p.id === d.id);
+                selectedNode.classed("hoverCircleNodeClass", false)
+                selectedNode.style("fill", (d) => d.color).style("stroke-width", 0);
+            })
+            .on("mouseout", (d) => {
+                let selectedNode = d3.selectAll("circle").filter(p => p.id === d.id);
+                selectedNode.style("fill", null).style("stroke-width", "3px");
+                selectedNode.classed("hoverCircleNodeClass", true);
+            });
 
-                let selectedNode = d3.selectAll("circle").
-                    filter(p => p.id === d.id)
-                selectedNode.classed("hoverCircleNodeClass",false)
-                    selectedNode.style("fill",(d)=>d.color).style("stroke-width",0)
-            })
-            .on("mouseout",(d)=>{
-               let selectedNode =  d3.selectAll("circle").
-                filter(p=>p.id === d.id)
-                selectedNode.style("fill",null).style("stroke-width","3px")
-                selectedNode.classed("hoverCircleNodeClass",true)
-            })
-            ;
         nodeEnter.
             append("circle").
-            attr("r", (d) => { return d.depth == 0 ? 1e-1 : 1e-4 })
-            // style("fill", (d) => d.color)
+            attr("r", (d) => { return d.depth == 0 ? 1e-1 : 1e-4 });
 
         nodeEnter.append("text").
             attr("x", fontPos.x).
             attr("y", fontPos.y).
             attr("text-anchor", "end")
             .text((d) => d.name)
-            .style("fill-opacity", 1e-6)
+            .style("fill-opacity", 1e-6);
 
         let nodeUpdate = nodeData.
             transition().
             duration(duration).
-            attr("transform", (d) => `translate(${d.y},${d.x})`)
+            attr("transform", (d) => `translate(${d.y},${d.x})`);
 
         nodeUpdate.select("circle")
             .attr("r", (d) => {
-                return d.depth === 0 ? 50 : 8
-            })
-
+                return d.depth === 0 ? 50 : 8;
+            });
 
         nodeUpdate.select("text")
             .style("fill-opacity", 1);
-        /**
-         * 
-         * write exit logic here 
-         *
-         */
+
+        // Remove any existing nodes
+            nodeData.exit().remove();
+
+        // // Update existing links
         let link = svg.selectAll("path.link")
             .data(links, (d) => d.target.id);
+
+        // Remove any exiting links
+        link.exit().remove();
 
         link.enter().insert("path", "g")
             .attr("class", "link")
@@ -106,19 +108,99 @@ $(document).ready(() => {
                 let o = { x: data.startPoint, y: 0 };
                 return diagonal({ source: o, target: o });
             });
+
         link.transition()
             .duration(duration)
             .attr("d", diagonal);
-
     }
 
-    function click(d) {
-        
-    } 
+    function click(d, data) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
+        }
+        updateTree(data);
+    }
 
-    function updateTree(){
+    function updateTree(data) {
+        let tree = d3.layout.tree().nodeSize([20, 30]).size([
+            dim.height - margin.top - margin.bottom, dim.width - margin.left - margin.right
+        ]);
 
-        
+        let nodes = tree.nodes(data).reverse();
+        let links = tree.links(nodes);
+        let diagonal = d3.svg.diagonal().projection((d) => [d.y, d.x]);
+        nodes.forEach((d) => d.y = d.depth * 110);
+
+        let svg = d3.select("body svg g");
+
+        // Update existing nodes
+        let nodeData = svg.selectAll("g.node").data(nodes, (d) => d.id || (d.id = ++i));
+
+        // Remove any exiting nodes
+        nodeData.exit().remove();
+
+        let nodeEnter = nodeData.enter().append("g")
+            .attr("class", "node")
+            .attr("transform", (d) => `translate(${d.y},${d.x})`)
+            .on("click", (d) => click(d, data))
+            .on("mouseover", (d) => {
+                let selectedNode = d3.selectAll("circle").filter(p => p.id === d.id);
+                selectedNode.classed("hoverCircleNodeClass", false)
+                selectedNode.style("fill", (d) => d.color).style("stroke-width", 0);
+            })
+            .on("mouseout", (d) => {
+                let selectedNode = d3.selectAll("circle").filter(p => p.id === d.id);
+                selectedNode.style("fill", null).style("stroke-width", "3px");
+                selectedNode.classed("hoverCircleNodeClass", true);
+            });
+
+        nodeEnter.append("circle")
+            .attr("r", (d) => d.depth === 0 ? 50 : 8);
+
+        nodeEnter.append("text")
+            .attr("x", fontPos.x)
+            .attr("y", fontPos.y)
+            .attr("text-anchor", "end")
+            .text((d) => {
+                return(d.name)
+            })
+            .style("fill-opacity", 1e-6);
+        let nodeUpdate = nodeData.
+            transition().
+            duration(duration).
+            attr("transform", (d) => `translate(${d.y},${d.x})`);
+
+        nodeUpdate.select("circle")
+            .attr("r", (d) => {
+                return d.depth === 0 ? 50 : 8;
+            });
+
+        nodeUpdate.select("circle")
+            .attr("r", (d) => d.depth === 0 ? 50 : 8);
+
+        nodeUpdate.select("text")
+            .style("fill-opacity", 1);
+
+        // // Update existing links
+        let link = svg.selectAll("path.link").data(links, (d) => d.target.id);
+
+        // // Remove any exiting links
+        link.exit().remove();
+
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", function (d) {
+                console.log("---",d)
+                let o = { x: d.x, y: d.y};
+                return diagonal({ source: o, target: o });
+            });
+
+        link.transition()
+            .duration(duration)
+            .attr("d", diagonal);
     }
 })
-
